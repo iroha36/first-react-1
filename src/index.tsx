e95ca66aabdea36a2526d0b03057f4a1c3e41587
iroha168 <kimura.masatoshi74@gmail.com>
2025-02-12 13:13:47 +0900

    add css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css'; // <-- CSSをインポート
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
